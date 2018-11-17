c1d6fbe5a6b243fe646891ee39355225aa4ba803
Simon <simon@smith77.co.uk>
2018-11-17 19:29:03 +0000

    Simon/unit tests (#53)
    * Unit tests for obfboard
    
    * Write out test-results and some tests for obfboard
    
    * More unit tests

--- src/app/obfboard.spec.ts
+++ src/app/obfboard.spec.ts
@@ -3,9 +3,97 @@ import { TestBed, inject } from '@angular/core/testing';
 import { OBFBoard, Grid, Button, Image, Sound, LoadBoardAction } from './obfboard';
 
 describe('OBFBoard', () => {
+
+  const testBoard = new OBFBoard().deserialize({
+    format: 'board_format',
+    id: 5,
+    locale: 'en_GB',
+    name: 'board_name',
+    description_html: '<b>desc</b>',
+    grid: {
+      rows: 2,
+      columns: 2,
+      order: [[1, null],
+      [null, 2]]
+    },
+    buttons: [
+      {
+        id: 1,
+        label: 'button1'
+      },
+      {
+        id: 2,
+        label: 'button2'
+      }
+    ],
+    images: [
+      {
+        id: 1,
+        url: 'http://a.b'
+      }
+    ],
+    sounds: [
+      {
+        id: 1,
+        url: 'http://b.c'
+      }
+    ]
+  });
+
   it('should be created', () => {
     const board = new OBFBoard();
     expect(board).toBeTruthy();
+  });
+
+  it('should deserialise', () => {
+    const board = testBoard;
+    expect(board).toBeTruthy();
+    expect(board.format).toBe('board_format');
+    expect(board.id).toBe('5');
+    expect(board.locale).toBe('en_GB');
+    expect(board.name).toBe('board_name');
+    expect(board.descriptionHtml).toBe('<b>desc</b>');
+    expect(board.grid.rows).toBe(2);
+    expect(board.grid.columns).toBe(2);
+    expect(board.grid.order).toEqual([['1', undefined], [undefined, '2']]);
+  });
+
+  it('should handle null button id', () => {
+    const board = testBoard;
+    expect(board.getButton(null)).toBe(null);
+  });
+
+  it('should handle null image id', () => {
+    const board = testBoard;
+    expect(board.getImage(null)).toBe(null);
+  });
+
+  it('should handle null sound id', () => {
+    const board = testBoard;
+    expect(board.getSound(null)).toBe(null);
+  });
+
+  it('should provide buttons', () => {
+    const board = testBoard;
+    const butt = board.getButton('2');
+    expect(butt).toBeTruthy();
+    expect(butt.label).toBe('button2');
+    expect(butt.parent).toBe(board);
+  });
+
+  it('should provide images', () => {
+    const board = testBoard;
+    const image = board.getImage('1');
+    expect(image).toBeTruthy();
+    expect(image.url).toBe('http://a.b');
+    expect(image.parent).toBe(board);
+  });
+
+  it('should provide sounds', () => {
+    const board = testBoard;
+    const sound = board.getSound('1');
+    expect(sound).toBeTruthy();
+    expect(sound.url).toBe('http://b.c');
   });
 });
 
@@ -28,10 +116,6 @@ describe('obfboard.Grid', () => {
     expect(grid.columns).toBe(2);
     // all ids should be strings now
     expect(grid.order).toEqual([['1', 'b1'], [undefined, 'b2'], ['5', '6']]);
-    for (const row of grid.order) {
-      expect(row.length).toBe(grid.columns);
-    }
-    expect(grid.order.length).toBe(grid.rows);
   });
 });
 
