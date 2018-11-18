67543653916888a5ba7feea7ff23e4afa4fa98f5
psi1977 <psi1977@bitbucket.org>
2018-11-18 19:13:42 +0000

    Hmmmmm, I've got it working.
    https://stackoverflow.com/questions/36919399/angular-2-view-not-updating-after-model-changes
    Not entirely happy.  It's quite slow to undisable.  Maybe just do some
    styling rather than disabling

--- src/app/speechbar/speechbar.component.ts
+++ src/app/speechbar/speechbar.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ConfigService, ButtonDisplayConfig } from '../config.service';
 import { SpeechbarService } from '../speechbar.service';
-import { BoardService } from '../board.service';
 
 @Component({
   selector: 'app-speechbar',
@@ -13,25 +12,27 @@ export class SpeechbarComponent implements OnInit {
   private _showIconsInSpeechbar: boolean;
   speaking: boolean;
 
-  constructor(private speechbarService: SpeechbarService, private config: ConfigService,
-              private boardService: BoardService) { }
+  constructor(
+    private speechbarService: SpeechbarService,
+    private config: ConfigService,
+    private cdRef: ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
     this._displayedButtons = this.config.displayedButtons;
     this._showIconsInSpeechbar = this.config.showIconsInSpeechbar;
-    this.speechbarService.getSpeaking().subscribe(this.updateSpeaking);
+    this.speechbarService.getSpeaking().subscribe(speaking => {
+      this.speaking = speaking;
+      this.cdRef.detectChanges();
+    });
   }
 
-  get displayedButtons() : ButtonDisplayConfig {
+  get displayedButtons(): ButtonDisplayConfig {
     return this._displayedButtons;
   }
 
-  get showIconsInSpeechbar() : boolean {
+  get showIconsInSpeechbar(): boolean {
     return this._showIconsInSpeechbar;
-  }
-
-  private updateSpeaking = (speaking: boolean) => {
-    this.speaking = speaking;
   }
 
   speechbarClick(): void {
