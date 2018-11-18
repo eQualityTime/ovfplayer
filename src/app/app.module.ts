16663a347041d88d2977d4aa648a526862f08457
psi1977 <psi1977@bitbucket.org>
2018-11-18 13:03:11 +0000

    Remove page turn directive as there is a better way!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import { ButtonPageComponent } from './button-page/button-page.component';
 import { ConfigPageComponent } from './config-page/config-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ObfButtonComponent } from './obf-button/obf-button.component';
-import { PageTurnerDirective } from './page-turner.directive';
 import { SpeechbarComponent } from './speechbar/speechbar.component';
 import { SafePipe } from './safe.pipe';
 
@@ -31,8 +30,7 @@ import { SafePipe } from './safe.pipe';
     ConfigPageComponent,
     MainPageComponent,
     SafePipe,
-    ObfButtonComponent,
-    PageTurnerDirective
+    ObfButtonComponent
   ],
   imports: [
     BrowserModule,
