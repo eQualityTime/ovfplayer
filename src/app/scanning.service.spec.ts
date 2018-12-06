b64c9ecd2bfe60ae21c05bac1adb0a629c8dd4c0
Clare Walsh <clare@prowler.io>
2018-12-06 19:52:33 +0000

    MOAR TESTS!!

--- src/app/scanning.service.spec.ts
+++ src/app/scanning.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { ScanningService, ScanningModel, ScannableCollection, ScannableCollectionProvider } from './scanning.service';
+import { ScanningService, ScanningModel, ScannableCollection, ScannableCollectionProvider, Scannable } from './scanning.service';
 import { ConfigService } from './config.service';
 import { Subscriber } from 'rxjs';
 
@@ -140,18 +140,154 @@ describe('ScanningService', () => {
     })();
   });
 
-  // it('should select an item on interaction', done => {
-  //   inject([ScanningService, ConfigService], (service: ScanningService, config: ConfigService) => {
-  //     config.scanningConfig.enabled = true;
-  //     const sub = service.getScanningModel().subscribe(new TestProvider([],
-  //       (scanningModel: ScanningModel) => {
-  //         expect(scanningModel).toBeTruthy();
-  //         sub.unsubscribe();
-  //         done();
-  //       }
-  //     ));
-  //   })();
-  // });
+  it('should select an item on interaction', done => {
+    inject([ScanningService, ConfigService], (service: ScanningService, config: ConfigService) => {
+      config.scanningConfig.enabled = true;
+      let count = 0;
+      const rows = [
+        new ScannableCollection(0, 'a'),
+        new ScannableCollection(1, 'b')
+      ];
+
+      const sub = service.getScanningModel().subscribe(new TestProvider(rows,
+        (scanningModel: ScanningModel) => {
+          if (count === 1) {
+            service.handleInteraction();
+          }
+
+          if (count === 2) {
+            expect(scanningModel.currentHighlight).toBe(undefined);
+            expect(scanningModel.currentSelection).toBe(rows[1]);
+            sub.unsubscribe();
+            done();
+          }
+
+          count++;
+        }
+      ));
+    })();
+  });
+
+  it('should clear selection on the next tick', done => {
+    inject([ScanningService, ConfigService], (service: ScanningService, config: ConfigService) => {
+      config.scanningConfig.enabled = true;
+      let count = 0;
+      const rows = [
+        new ScannableCollection(0, 'a'),
+        new ScannableCollection(1, 'b')
+      ];
+
+      const sub = service.getScanningModel().subscribe(new TestProvider(rows,
+        (scanningModel: ScanningModel) => {
+          if (count === 1) {
+            service.handleInteraction();
+          }
+
+          if (count === 3) {
+            expect(scanningModel.currentSelection).toBe(undefined);
+            sub.unsubscribe();
+            done();
+          }
+
+          count++;
+        }
+      ));
+    })();
+  });
+
+  it('should restart scanning if selection has no children', done => {
+    inject([ScanningService, ConfigService], (service: ScanningService, config: ConfigService) => {
+      config.scanningConfig.enabled = true;
+      let count = 0;
+      const rows = [
+        new ScannableCollection(0, 'a'),
+        new ScannableCollection(1, 'b')
+      ];
+
+      const sub = service.getScanningModel().subscribe(new TestProvider(rows,
+        (scanningModel: ScanningModel) => {
+          if (count === 0 && ++count) { // inline for atomicity
+            service.handleInteraction();
+          } else {
+            if (scanningModel.currentHighlight !== undefined) {
+              expect(scanningModel.currentHighlight).toBe(rows[count - 1]);
+              count++;
+            }
+          }
+
+          if (count > rows.length) {
+            sub.unsubscribe();
+            done();
+          }
+        }
+      ));
+    })();
+  });
+
+  it('should scan children if selection has children', done => {
+    inject([ScanningService, ConfigService], (service: ScanningService, config: ConfigService) => {
+      config.scanningConfig.enabled = true;
+      let count = 0;
+      const rows = [
+        new ScannableCollection(0, 'a'),
+        new ScannableCollection(1, 'b')
+      ];
+
+      rows[0].addChild(new Scannable(0, 'c'));
+      rows[0].addChild(new Scannable(1, 'd'));
+
+      const sub = service.getScanningModel().subscribe(new TestProvider(rows,
+        (scanningModel: ScanningModel) => {
+          if (count === 0 && ++count) { // inline for atomicity
+            service.handleInteraction();
+          } else {
+            if (scanningModel.currentHighlight !== undefined) {
+              expect(scanningModel.currentHighlight).toBe(rows[0].getChildren()[count - 1]);
+              count++;
+            }
+          }
+
+          if (count > rows.length) {
+            sub.unsubscribe();
+            done();
+          }
+        }
+      ));
+    })();
+  });
+
+  it('should iterate over children repeatedly', done => {
+    inject([ScanningService, ConfigService], (service: ScanningService, config: ConfigService) => {
+      config.scanningConfig.enabled = true;
+      let count = 0;
+      const rows = [
+        new ScannableCollection(0, 'a'),
+        new ScannableCollection(1, 'b')
+      ];
+
+      rows[0].addChild(new Scannable(0, 'c'));
+      rows[0].addChild(new Scannable(1, 'd'));
+
+      const sub = service.getScanningModel().subscribe(new TestProvider(rows,
+        (scanningModel: ScanningModel) => {
+          if (count === 0 && ++count) { // inline for atomicity
+            service.handleInteraction();
+          } else {
+            if (scanningModel.currentHighlight !== undefined) {
+              const index = count > rows.length ? count - rows.length : count;
+              expect(scanningModel.currentHighlight).toBe(rows[0].getChildren()[index - 1]);
+              count++;
+            }
+          }
+
+          if (count > rows.length * 2) {
+            sub.unsubscribe();
+            done();
+          }
+        }
+      ));
+    })();
+  });
 });
 
 class TestProvider extends Subscriber<ScanningModel> implements ScannableCollectionProvider {
