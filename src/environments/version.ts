e5cd138951feea7d5326f23e96fff3dc029ceadb
psi1977 <psi1977@bitbucket.org>
2018-12-16 21:50:52 +0000

    Make version dev only for now

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,14 +1,6 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "20181216.2-0-g5bc1f3b-dirty",
-    "hash": "g5bc1f3b",
-    "distance": 0,
-    "tag": "20181216.2",
-    "semver": null,
-    "suffix": "0-g5bc1f3b-dirty",
-    "semverString": "",
-    "version": "0.1.0"
+    "tag": "DEV ONLY"
 };
 /* tslint:enable */
