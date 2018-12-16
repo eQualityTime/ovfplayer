5bc1f3b3d383fc8ce04cce5912ab8c997d7b9f37
psi1977 <psi1977@bitbucket.org>
2018-12-16 21:02:17 +0000

    Remove release it

---
+++ src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "880130a-dirty",
+    "hash": "880130a",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "880130a-dirty",
+    "semverString": null,
+    "version": "0.0.0"
+};
+/* tslint:enable */
