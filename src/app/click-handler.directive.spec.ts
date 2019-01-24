20e73bb75beda32fb69ec80b5ac060dcc234e349
Clare Walsh <clare@prowler.io>
2019-01-24 14:47:00 +0000

    Added more tests to spy on directive

--- src/app/click-handler.directive.spec.ts
+++ src/app/click-handler.directive.spec.ts
@@ -13,9 +13,10 @@ class TestClickHandlerComponent {
   }
 }
 
-describe('ClickHandlerDirective on click', () => {
+describe('ClickHandlerDirective', () => {
 
   let component: TestClickHandlerComponent;
+  let directive: ClickHandlerDirective;
   let fixture: ComponentFixture<TestClickHandlerComponent>;
   let configServiceStub: Partial<ConfigService>;
 
@@ -38,12 +39,20 @@ describe('ClickHandlerDirective on click', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TestClickHandlerComponent);
     component = fixture.componentInstance;
+    const directiveEl = fixture.debugElement.query(By.directive(ClickHandlerDirective));
+    directive = directiveEl.injector.get(ClickHandlerDirective);
+
     spyOn(component, 'testHandler');
+    spyOn(directive, 'handleEvent').and.callThrough();
+    spyOn(directive, 'onClick').and.callThrough();
+    spyOn(directive, 'onPress').and.callThrough();
+    spyOn(directive, 'onRelease').and.callThrough();
     fixture.detectChanges();
   });
 
   it('should create an instance', () => {
     expect(component).toBeTruthy();
+    expect(directive).toBeTruthy();
   });
 
   it('should fire on configured trigger', fakeAsync( () => {
@@ -52,6 +61,7 @@ describe('ClickHandlerDirective on click', () => {
     div.triggerEventHandler('click', null);
     tick();
     fixture.detectChanges();
+    expect(directive.handleEvent).toHaveBeenCalled();
     expect(component.testHandler).toHaveBeenCalled();
   }));
 
@@ -62,6 +72,37 @@ describe('ClickHandlerDirective on click', () => {
     div.triggerEventHandler('mouseup', null);
     tick();
     fixture.detectChanges();
+    expect(directive.handleEvent).toHaveBeenCalled();
     expect(component.testHandler).not.toHaveBeenCalled();
+  }));
+
+  it('should handle moousedown', fakeAsync( () => {
+    fixture.detectChanges();
+    const div = fixture.debugElement.query(By.css('div'));
+    div.triggerEventHandler('mousedown', null);
+    tick();
+    fixture.detectChanges();
+    expect(directive.onPress).toHaveBeenCalled();
+    expect(directive.handleEvent).toHaveBeenCalled();
+  }));
+
+  it('should handle moouseup', fakeAsync( () => {
+    fixture.detectChanges();
+    const div = fixture.debugElement.query(By.css('div'));
+    div.triggerEventHandler('mouseup', null);
+    tick();
+    fixture.detectChanges();
+    expect(directive.onRelease).toHaveBeenCalled();
+    expect(directive.handleEvent).toHaveBeenCalled();
+  }));
+
+  it('should handle click', fakeAsync( () => {
+    fixture.detectChanges();
+    const div = fixture.debugElement.query(By.css('div'));
+    div.triggerEventHandler('click', null);
+    tick();
+    fixture.detectChanges();
+    expect(directive.onClick).toHaveBeenCalled();
+    expect(directive.handleEvent).toHaveBeenCalled();
   }));
 });
